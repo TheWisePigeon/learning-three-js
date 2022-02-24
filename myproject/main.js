4e195065e488d4fc03fd1dc5d23924fa885376b1
TheWisePigeon <zozozozeph@gmail.com>
2022-02-24 22:24:54 +0000

    Update main.js

--- myproject/main.js
+++ myproject/main.js
@@ -4,4 +4,8 @@ import *  as THREE from 'three'
 const scene = new THREE.Scene()
 
 
-const camera = new THREE.PerspectiveCamera()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
